a2f17d1cac1c3f1ce55c2af2d2919c6f0d9fe1bf
MarianoGiayetto <marianogiayetto@gmail.com>
2022-10-15 19:02:49 -0300

    ocultar modal al finalizar compra

--- app.js
+++ app.js
@@ -284,7 +284,9 @@ function comprar(e){
     },
     willClose: () => {
       clearInterval(timerInterval)
-      window.location.href = "./index.html"
+      const modalResumen=document.getElementById('exampleModal2')
+      const modal = bootstrap.Modal.getInstance(modalResumen)
+      modal.hide()
     }
   })    
   carrito = {}  
